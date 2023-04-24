a296be3ad68efec5e2bbfe9d783dd9b425c1ef8c
OkosodoNorah <ksdnorah@gmail.com>
2023-04-23 23:32:56 -0700

    team

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -26,6 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -52,7 +53,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
@@ -63,4 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
